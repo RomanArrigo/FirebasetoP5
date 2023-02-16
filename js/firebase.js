ddec007ef36fbda06221db14c739e8f4f75dfe55
RomanArrigo <93398234+RomanArrigo@users.noreply.github.com>
2023-02-16 12:57:41 +0000

    Add files via upload
    initial commit

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBbNIquPTpWqR3irQ1gv_DpKFvB_wJVQt0",
+  authDomain: "fir-top5testroman.firebaseapp.com",
+  projectId: "fir-top5testroman",
+  storageBucket: "fir-top5testroman.appspot.com",
+  messagingSenderId: "198636453431",
+  appId: "1:198636453431:web:7306ba99196da7eaae49b2"
+});
+
+var db = firebase.firestore()
